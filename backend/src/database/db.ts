747fd06a0e4da98b2e591f1c21064b5e739356fd
Dimitris Georgiadis <geojimas@gmail.com>
2022-03-28 18:07:09 +0300

    update dependencies

--- backend/src/database/db.ts
+++ backend/src/database/db.ts
@@ -1,22 +1,18 @@
-import mongoose from 'mongoose'
+import { connect } from 'mongoose'
 import * as dotenv from 'dotenv'
 import console from 'console'
 dotenv.config()
 
-export const connectDB = async () => {
+export const connectDB = async (): Promise<void> => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI as string, {
-      useCreateIndex: true,
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-    })
+    const conn = await connect(process.env.MONGO_URI as string)
+
     console.log(`Host: ${conn.connection.host}`)
     console.log(`Name: ${conn.connection.name}`)
     console.log(`Port: ${conn.connection.port}`)
     console.log('-----------------------------')
     console.log('MongoDB connected Successfully!')
-    
+
   } catch (error) {
     console.log(error)
     process.exit(1)

ea50801a8d69a2543ff5c9747f48fc27a358b96b
Dimitris Georgiadis <geojimas@gmail.com>
2022-05-10 00:21:57 +0300

    users name added on navbar

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -4,24 +4,36 @@
       <img width="30" height="30" src="/logo.png" />
       <h1 class="text-xl">Gromet</h1>
     </router-link>
-    <ul
+    <div v-if="isAuth" class="flex items-center justify-center">
+      <div class="-space-x-4">
+        <img
+          class="relative z-30 inline object-cover w-12 h-12 border-2 border-white bg-white rounded-full"
+          src="/avatar.png"
+          alt="Profile image" />
+        <div
+          class="inline-flex items-center justify-center px-8 py-2 text-md font-bold leading-none text-red-100 bg-purple-700 rounded-full">
+          {{ user.name }}
+        </div>
+      </div>
+    </div>
+    <div
       v-if="isAuth"
       class="md:flex md:items-center md:px-0 text-center md:pb-0 md:static bg-gray-900 md:w-auto w-full duration-500 ease-in">
-      <li v-for="(link, index) in privateRoutes" :key="index" class="md:mx-4 md:my-0 my-4 text-xl">
+      <div v-for="(link, index) in privateRoutes" :key="index" class="md:mx-4 md:my-0 my-4 text-xl">
         <router-link :to="link.link" class="hover:text-green-400 hover:underline">{{
           link.name
         }}</router-link>
-      </li>
+      </div>
       <button
         @click="logout"
-        class="inline-flex items-center justify-center py-1.5 px-4  bg-red-600 border border-transparent rounded-md font-semibold capitalize text-white hover:bg-red-700 active:bg-red-700 focus:outline-none focus:border-red-700 focus:ring focus:ring-red-200 disabled:opacity-25 transition">
+        class="inline-flex items-center justify-center py-1.5 px-4 bg-red-600 border border-transparent rounded-md font-semibold capitalize text-white hover:bg-red-700 active:bg-red-700 focus:outline-none focus:border-red-700 focus:ring focus:ring-red-200 disabled:opacity-25 transition">
         Logout
       </button>
-    </ul>
-    <ul
+    </div>
+    <div
       v-else
       class="md:flex md:items-center md:px-0 text-center md:pb-0 md:static bg-gray-900 md:w-auto w-full duration-500 ease-in">
-      <li v-for="(link, index) in publicRoutes" :key="index" class="md:mx-4 md:my-0 my-4 text-xl">
+      <div v-for="(link, index) in publicRoutes" :key="index" class="md:mx-4 md:my-0 my-4 text-xl">
         <router-link
           :to="link.link"
           :class="{
@@ -31,8 +43,8 @@
           }"
           >{{ link.name }}</router-link
         >
-      </li>
-    </ul>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -47,6 +59,7 @@ const store = userStore()
 const router = useRouter()
 
 const isAuth = computed(() => store.isLoggedIn)
+const user = computed(() => store.userInfo)
 
 const toast = useToast()
 
--- client/src/views/HomePage.vue
+++ client/src/views/HomePage.vue
@@ -1,10 +1,14 @@
 <template>
-  <div class="flex justify-center m-20 items-center flex-col animate__animated animate__zoomIn">
-    <h1>Welcome Page</h1>
-    <h2 class="mt-40">
-      JWT authentication with NodeJS/TypeScript in Backend and VueJS/JavaScript in frontend.
-    </h2>
-    <h2>Try Register and Login Actions.</h2>
-    <h2>And try to access Dashboard Page with and without authorization.</h2>
+  <div class="animate__animated animate__zoomIn">
+    <div class="flex justify-center m-20 items-center flex-col">
+      <h1 class="text-3xl font-bold">Welcome Page</h1>
+      <h2 class="mt-20 text-xl">
+        JWT authentication with NodeJS/TypeScript in Backend and VueJS/JavaScript in frontend.
+      </h2>
+      <div class="text-center m-4 text-xl">
+        <p>Try Register and Login Actions.</p>
+        <p>And try to access Dashboard Page with and without authorization.</p>
+      </div>
+    </div>
   </div>
 </template>

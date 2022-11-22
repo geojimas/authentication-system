8781cb36f603b6fe4ee3f3f94b984a37da7cf5da
Dimitris Georgiadis <geojimas@gmail.com>
2022-11-22 14:09:38 +0200

    fix prod url

--- client/utils/axios.js
+++ client/utils/axios.js
@@ -1,12 +1,14 @@
 import axios from 'axios'
+import * as dotenv from 'dotenv'
+dotenv.config()
 
 export const BASE_API_URLS = [
   { name: 'Localhost', endpoint: 'http://localhost:5000/api' },
-  { name: 'Production', endpoint: 'https://climax245.herokuapp.com/api' }
+  { name: 'Production', endpoint: 'https://climax245.up.railway.app/api' }
 ]
 
 const api = axios.create({
-  baseURL: BASE_API_URLS[0].endpoint,
+  baseURL: process.env.NODE_ENV === 'Localhost' ? BASE_API_URLS[0].endpoint : BASE_API_URLS[1].endpoint,
   timeout: 1000,
   withCredentials: true,
   headers: { 'Content-Type': 'application/json' }
